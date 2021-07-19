89801afc7c7613d7735b999a25e384fc512e9f2b
kristophm <k@kristophmatthews.com>
2021-07-19 14:40:23 -0400

    contract management

--- contracts/ZorroStrategy.sol
+++ contracts/ZorroStrategy.sol
@@ -2,9 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ZorroStrategy {
+    /* Constants */
+
     /* Variables */
     // Key addresses / ownership
     address owner;
+    // Strategies
+    mapping(string => address[]) public lpContracts;
+
 
     // Financial paramters
     uint16 public compoundingFrequencyDays;
@@ -18,6 +23,7 @@ contract ZorroStrategy {
     // mapping (address => uint256) public balances;
 
     // Events
+    event AddedContract(string strategy, address contractAddress);
 
     // Modifiers
     modifier onlyOwner {
@@ -37,6 +43,16 @@ contract ZorroStrategy {
 
     // State-changing methods
     function invest() external payable {
+        // Requirements
+        require(msg.value > 0, "Message must have a value greater than 0");
+
+        // Iterates through accepted LP contracts for this strategy (use oracle)
+        // ---- OR, should they be updated into the contract? 
+        // -- Perform swaps to get all required underlying tokens (Use oracle for fair pricing)
+
+        // -- Engage in each LP contract
+
+        // -- Store received LP token in Zorro contract
 
     }
 
@@ -49,11 +65,50 @@ contract ZorroStrategy {
     }
 
     function setCompoundingFrequency(uint16 minElapsedDays) external onlyOwner {
+        // Sets the max frequency of compounding (and thus profit-taking) to a value in days.
         compoundingFrequencyDays = minElapsedDays;
     }
 
     function setZorroPerformanceFeePercent(uint8 feePct) external onlyOwner {
+        // Sets performance fee percentage to a new value.
         require(feePct >= 0 && feePct <= 100, "Percent must be a value between 0 and 100.");
         performanceFeePercent = feePct;
+    }
+
+    function changeOwner(address newOwner) external onlyOwner {
+        // Changes the owner of this contract
+        owner = newOwner;
+    }
+
+    function addLPContractToStrategy(address contractAddress, string memory strategy) external onlyOwner {
+        // Get current contracts associated with strategy provided
+        address[] storage strategyContracts = lpContracts[strategy];
+        // Check to make sure contract doesn't already exist
+        for (uint16 i=0;i<strategyContracts.length;i++) {
+            if (strategyContracts[i] == contractAddress) {
+                require(false, "Contract address already exists for this strategy");
+            }
+        }
+        // Add contract & notify
+        strategyContracts.push(contractAddress);
+        emit AddedContract(strategy, contractAddress);
+    }
+
+    function removeLPContractFromStrategy(address contractAddress, string memory strategy) external onlyOwner {
+        // Get current contracts associated with strategy provided
+        address[] storage strategyContracts = lpContracts[strategy];
+        // Check to make sure contract exists
+        int16 matchingIndex = -1;
+        for (uint16 i=0;i<strategyContracts.length;i++) {
+            if (strategyContracts[i] == contractAddress) {
+                matchingIndex = int16(i);
+                break;
+            }
+        }
+        // If does not exist, revert
+        require(matchingIndex >= 0, "Matching contract address could not be found for this strategy");
+        // If does exist, remove contract and notify
+        delete strategyContracts[uint16(matchingIndex)];
+        emit AddedContract(strategy, contractAddress);
     }
 }
